69b83907505688d0219dbd25ddb9f855a5005c64
Wesley Antico <Wesley@Wesleys-MacBook-Air.local>
2024-07-11 19:24:52 -0400

    styling with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./assets/**/*.{html,js}'],
+  theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
